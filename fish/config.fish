57093d6cd21027c80baca5949164321a79d7b6ed
sourlemon11 <soneen11@yahoo.com>
2021-01-31 23:59:14 -0500

    pyenv

--- fish/config.fish
+++ fish/config.fish
@@ -148,3 +148,9 @@ set -x LESS_TERMCAP_se (printf "\033[0m")
 set -x LESS_TERMCAP_so (printf "\033[01;44;33m")
 set -x LESS_TERMCAP_ue (printf "\033[0m")
 set -x LESS_TERMCAP_us (printf "\033[01;32m")
+
+
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+  pyenv init - | source
+end
