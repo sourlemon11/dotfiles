5ec2decf6111391914e12c4e41481b513a8127ff
sourlemon11 <soneen11@yahoo.com>
2021-08-15 20:16:55 -0400

    added clips mode

--- emacs/spacemacs/config/_languages.el
+++ emacs/spacemacs/config/_languages.el
@@ -2,4 +2,5 @@
 
 (require '_latex)      ;; ~/.config/spacemacs/_latex.el
 (require '_javascript) ;; ~/.config/spacemacs/_javascript.el
-(require '_python)     ;; ~/.config/spacemacs/_python.el
+(require '_clips) ;; ~/.config/spacemacs/_clips.el
+;; (require '_python)     ;; ~/.config/spacemacs/_python.el
